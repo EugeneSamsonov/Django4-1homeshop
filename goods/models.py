bed4c13589e90b51790a2dd99ec759160be19884
EugeneSamsonov <samsonovevgenii231@gmail.com>
2024-07-12 13:19:46 +0300

    Add pagination

--- goods/models.py
+++ goods/models.py
@@ -32,6 +32,7 @@ class Products(models.Model):
         db_table = 'product'
         verbose_name = "продукт"
         verbose_name_plural = "Продукты"
+        ordering = ("id", )
     
 
     def __str__(self) -> str:

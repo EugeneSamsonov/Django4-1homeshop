5a209a1dabafbbf7c01d72bd5093c31baf2285c0
EugeneSamsonov <samsonovevgenii231@gmail.com>
2024-07-15 14:39:10 +0300

    Add PostgreSQL database and fullText search

--- goods/utils.py
+++ goods/utils.py
@@ -1,7 +1,9 @@
 
 
-from django.db.models import Q
+# from django.db.models import Q
 from goods.models import Products
+
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 
 def q_search(query):
@@ -9,13 +11,17 @@ def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=query)
     
-    keywords = [word for word in query.split() if len(word) > 2]
+    vector = SearchVector("name", "description")
+    query = SearchQuery(query)
 
-    q_objs = Q()
+    return Products.objects.annotate(rank=SearchRank(vector, query)).filter(rank__gt=0).order_by("-rank")
+
+    # keywords = [word for word in query.split() if len(word) > 2]
+
+    # q_objs = Q()
     
-    for keyword in keywords:
-        q_objs |= Q(name__icontains=keyword)
-        q_objs |= Q(description__icontains=keyword)
+    # for keyword in keywords:
+    #     q_objs |= Q(name__icontains=keyword)
+    #     q_objs |= Q(description__icontains=keyword)
     
-    
-    return Products.objects.filter(q_objs)
+    # return Products.objects.filter(q_objs)

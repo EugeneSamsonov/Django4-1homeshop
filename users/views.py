1e91c75424691b510a043f10dfef115c14b81490
EugeneSamsonov <samsonovevgenii231@gmail.com>
2024-07-18 16:00:22 +0300

    Add registration form and controller

--- users/views.py
+++ users/views.py
@@ -1,29 +1,54 @@
-from django.shortcuts import render
+from django.contrib import auth
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.urls import reverse
+
+from users.forms import UserLoginForm, UserRegistrationForm
 
 # Create your views here.
 
 
 def login(request):
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserLoginForm()
+
     context = {
-        'title': 'Вход'
+        'title': 'Вход',
+        'form': form
     }
     
     return render(request, 'users/login.html', context)
 
 
 def registration(request):
-    context = {
-        'title': 'Регистрация'
-    }
+    if request.method == "POST":
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
 
+    context = {
+        'title': 'Вход',
+        'form': form
+    }
+    
     return render(request, 'users/registration.html', context)
 
 
 def logout(request):
-    context = {
-        'title': 'Выход'
-    }
-    return render(request, 'users/logout.html', context)
+    auth.logout(request)
+    return redirect(reverse("main:index"))
 
 def profile(request):
     context = {
